06e8a84a45ae037d67a246cf73cd8809c88266ed
Naveen HK <hknsagar@gmail.om>
2023-05-09 02:00:21 +0530

    Updates

--- src/components/Button.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Button = ({name}) => {
-  return <button className=" bg-gray-100 px-3 py-1 rounded-lg m-2 text-sm">{name}</button>;
-};
-
-export default Button;
--- src/store/store.js
+++
@@ -1,7 +0,0 @@
-import searchCacheSlice from "./searchCacheSlice";
-
-const { configureStore } = require("@reduxjs/toolkit");
-
-const store = configureStore({ reducer: { searchCache: searchCacheSlice } });
-
-export default store;
